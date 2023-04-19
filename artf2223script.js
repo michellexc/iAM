c7b0e7dd1e0561ddda72331b7038b0ee04d42b37
michellexc <129274881+michellexc@users.noreply.github.com>
2023-04-19 02:30:02 -0400

    first commit

---
+++ artf2223script.js
@@ -0,0 +1,129 @@
+$('.button1').on('click',function(){
+    if($('.web1').css('display')!='none'){
+    	console.log("if runs");
+    $('.mobile1').show().siblings('.web1').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("else if runs");
+        $('.web1').show().siblings('.mobile1').hide();
+    }
+});
+
+$('.button2').on('click',function(){
+    if($('.web2').css('display')!='none'){
+    	console.log("2nd if runs");
+    $('.mobile2').show().siblings('.web2').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("2nd else if runs");
+        $('.web2').show().siblings('.mobile2').hide();
+    }
+});
+
+$('.button3').on('click',function(){
+    if($('.web3').css('display')!='none'){
+    	console.log("3rd if runs");
+    $('.mobile3').show().siblings('.web3').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("3rd else if runs");
+        $('.web3').show().siblings('.mobile3').hide();
+    }
+});
+
+$('.button4').on('click',function(){
+    if($('.web4').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile4').show().siblings('.web4').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web4').show().siblings('.mobile4').hide();
+    }
+});
+
+$('.button5').on('click',function(){
+    if($('.web5').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile5').show().siblings('.web5').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web5').show().siblings('.mobile5').hide();
+    }
+});
+
+$('.button6').on('click',function(){
+    if($('.web6').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile6').show().siblings('.web6').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web6').show().siblings('.mobile6').hide();
+    }
+});
+
+$('.button7').on('click',function(){
+    if($('.web7').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile7').show().siblings('.web7').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web7').show().siblings('.mobile7').hide();
+    }
+});
+
+$('.button8').on('click',function(){
+    if($('.web8').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile8').show().siblings('.web8').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web8').show().siblings('.mobile8').hide();
+    }
+});
+
+$('.button9').on('click',function(){
+    if($('.web9').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile9').show().siblings('.web9').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web9').show().siblings('.mobile9').hide();
+    }
+});
+
+$('.button10').on('click',function(){
+    if($('.web10').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile10').show().siblings('.web10').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web10').show().siblings('.mobile10').hide();
+    }
+});
+
+$('.button11').on('click',function(){
+    if($('.web11').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile11').show().siblings('.web11').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web11').show().siblings('.mobile11').hide();
+    }
+});
+
+$('.button12').on('click',function(){
+    if($('.web12').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile12').show().siblings('.web12').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web12').show().siblings('.mobile12').hide();
+    }
+});
+
+$('.button13').on('click',function(){
+    if($('.web13').css('display')!='none'){
+    	console.log("4th if runs");
+    $('.mobile13').show().siblings('.web13').hide();
+    }else if($('.text2').css('display')!='none'){
+    	console.log("4th else if runs");
+        $('.web13').show().siblings('.mobile13').hide();
+    }
+});
